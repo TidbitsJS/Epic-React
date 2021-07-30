2bd0db6c0f8eacb3b4b5492c9202bf6ef4cf861e
Tidbits JS <tidbitsjs@gmail.com>
2021-07-30 20:17:48 +0530

    react fundamentals setup

---
+++ epicreact/react-fundamentals/src/__tests__/05.js
@@ -0,0 +1,52 @@
+import {alfredTip} from '@kentcdodds/react-workshop-app/test-utils'
+import chalk from 'chalk'
+import {render, screen, prettyDOM} from '@testing-library/react'
+// import App from '../final/05'
+import App from '../exercise/05'
+
+test('renders the correct styles new', () => {
+  render(<App />)
+  const allBoxes = screen.getAllByText(/box/i)
+
+  const className = 'box'
+  allBoxes.forEach(box => {
+    alfredTip(
+      () => {
+        expect(box).toHaveClass(className)
+      },
+      () =>
+        `
+This box is missing the className "${className}"
+
+${chalk.reset(prettyDOM(box))}
+    `.trim(),
+    )
+  })
+
+  allBoxes.forEach(box => {
+    alfredTip(
+      () => {
+        expect(box).toHaveStyle('font-style: italic;')
+      },
+      () =>
+        `
+This box is missing fontStyle: 'italic' in the style prop
+
+${chalk.reset(prettyDOM(box))}
+    `.trim(),
+    )
+  })
+
+  const small = screen.getByText(/small/i)
+  const medium = screen.getByText(/medium/i)
+  const large = screen.getByText(/large/i)
+
+  expect(small).toHaveClass('box--small')
+  expect(small).toHaveStyle('background-color: lightblue;')
+
+  expect(medium).toHaveClass('box--medium')
+  expect(medium).toHaveStyle('background-color: pink;')
+
+  expect(large).toHaveClass('box--large')
+  expect(large).toHaveStyle('background-color: orange;')
+})
---
+++ epicreact/react-fundamentals/src/exercise/05.js
@@ -0,0 +1,50 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+// 💰 Use the className for the size and style (backgroundColor) for the color
+// 💰 each of the elements should also have the "box" className applied
+
+// 🐨 add a className prop to each of these and apply the correct class names
+// 💰 Here are the available class names: box, box--large, box--medium, box--small
+
+// 🐨 add a style prop to each of them as well so their background color
+// matches what the text says it should be as well as `fontStyle: 'italic'`
+const smallBox = (
+  <div
+    className="box box--small"
+    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+  >
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div
+    className="box box--medium"
+    style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+  >
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div
+    className="box box--large"
+    style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+  >
+    large orange box
+  </div>
+)
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+    </div>
+  )
+}
+
+export default App
