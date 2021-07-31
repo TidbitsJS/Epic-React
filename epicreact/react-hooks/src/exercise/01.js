67648856f216f6daac186c05dcca936fd296e8f2
Tidbits JS <tidbitsjs@gmail.com>
2021-07-31 09:29:43 +0530

    react-hooks setup

---
+++ epicreact/react-hooks/src/exercise/01.js
@@ -0,0 +1,31 @@
+// useState: greeting
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+function Greeting({initialName}) {
+  // 💣 delete this variable declaration and replace it with a React.useState call
+  // const name = ''
+  const [name, setName] = React.useState(initialName)
+
+  function handleChange(event) {
+    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting initialName="Tidbit" />
+}
+
+export default App
