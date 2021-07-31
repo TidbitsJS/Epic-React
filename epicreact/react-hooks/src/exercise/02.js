2e4b87af07076d02863ab46024ba4c3fa75dd6a4
Tidbits JS <tidbitsjs@gmail.com>
2021-07-31 20:13:51 +0530

    finished four lessons

--- epicreact/react-hooks/src/exercise/02.js
+++ epicreact/react-hooks/src/exercise/02.js
@@ -3,14 +3,34 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, defaultValue = '') {
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, name)
+  }, [key, name])
+
+  return [name, setName]
+}
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  // const [name, setName] = React.useState(
+  //   () => window.localStorage.getItem('name') || initialName,
+  // )
+
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
