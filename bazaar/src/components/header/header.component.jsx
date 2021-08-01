24be84c8a27cef8b9de8a6cb3c79ad1a64e0036c
Tidbits JS <tidbitsjs@gmail.com>
2021-08-01 23:09:38 +0530

    header component

---
+++ bazaar/src/components/header/header.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+import './header.styles.scss'
+
+const Header = () => {
+    return (
+        <div className="header">
+           <Link to="/" className="logo-container">
+                <Logo />
+           </Link> 
+           <div className="options">
+                <Link to="/shop" className="option">Shop</Link>
+                <Link to="/contact" className="option">Contact</Link>
+           </div>
+        </div>
+    )
+}
+
+export default Header
