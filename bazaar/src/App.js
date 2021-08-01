41942f311f9e3ad29099e2ad96601228fd6b2400
Tidbits JS <tidbitsjs@gmail.com>
2021-08-01 12:29:18 +0530

    node-sass@6 setup

---
+++ bazaar/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./homepage.component";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ bazaar/src/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import './homepage.styles.scss'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">WOMENs</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">MENs</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
