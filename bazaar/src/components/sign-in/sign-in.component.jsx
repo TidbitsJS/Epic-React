62d5386a854d3171d0013da55823646ff72027b7
Tidbits JS <tidbitsjs@gmail.com>
2021-08-03 22:23:48 +0530

    sign-in handleSubmit method update

--- bazaar/src/components/sign-in/sign-in.component.jsx
+++ bazaar/src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import CustomButton from '../custom-button/custom-button.component'
 import FormInput from '../form-input/form-input.component'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 import './sign-in.styles.scss'
 
@@ -15,9 +15,17 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault()
-        this.setState({email: '', password: ''})
+        const {email, password} = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email: '', password: ''})
+        }catch(error) {
+            console.error(error)
+        }
+
     }
 
     hadnleChange = event => {
