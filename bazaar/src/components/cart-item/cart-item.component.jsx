a18bafddc4ea8e6004e20ca526526ffff8f1b577
Tidbits JS <tidbitsjs@gmail.com>
2021-08-05 20:39:17 +0530

    cart-item component

---
+++ bazaar/src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./cart-item.styles.scss";
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt="cart-item-imageUrl" />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {" "}
+          {quantity} x {price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
