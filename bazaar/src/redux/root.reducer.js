bf3e822b8447072b1f5fa19b6c88c54e48116c06
Tidbits JS <tidbitsjs@gmail.com>
2021-08-11 21:53:29 +0530

    moving directory sections in redux store

--- bazaar/src/redux/root.reducer.js
+++ bazaar/src/redux/root.reducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
   key: "root",
@@ -13,6 +14,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
