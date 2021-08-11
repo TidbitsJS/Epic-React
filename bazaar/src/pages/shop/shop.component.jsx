a171cbb485d7c7c22dec17da495aa42823f3dce6
Tidbits JS <tidbitsjs@gmail.com>
2021-08-11 23:00:14 +0530

    moving shop collections to redux store

--- bazaar/src/pages/shop/shop.component.jsx
+++ bazaar/src/pages/shop/shop.component.jsx
@@ -1,27 +1,24 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data'
-import PreviewCollection from '../../components/preview-collection/preview-collection.component'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selectors";
+import PreviewCollection from "../../components/preview-collection/preview-collection.component";
 
- class ShopPage extends Component {
-     constructor(props) {
-         super(props)
-
-         this.state = {
-             collections: SHOP_DATA
-         }
-     }
-    render() {
-        const {collections} = this.state
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <PreviewCollection key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
+class ShopPage extends Component {
+  render() {
+    const { collections } = this.props;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <PreviewCollection key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default ShopPage
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
--- bazaar/src/redux/root.reducer.js
+++ bazaar/src/redux/root.reducer.js
@@ -4,6 +4,7 @@ import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
   key: "root",
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
