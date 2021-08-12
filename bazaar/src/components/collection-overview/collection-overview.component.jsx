ad90c2ae2d3252b51e1da58658c4c0b8f017e05f
Tidbits JS <tidbitsjs@gmail.com>
2021-08-13 00:07:00 +0530

    collection overview selector

--- bazaar/src/components/collection-overview/collection-overview.component.jsx
+++ bazaar/src/components/collection-overview/collection-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import PreviewCollection from "../preview-collection/preview-collection.component";
 
 import "./collection-overview.styles.scss";
@@ -17,7 +17,7 @@ const CollectionOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
