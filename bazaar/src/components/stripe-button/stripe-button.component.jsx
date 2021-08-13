01a8f986027f57c91237d261e6e42a11209107a1
Tidbits JS <tidbitsjs@gmail.com>
2021-08-13 20:28:22 +0530

    stripe payment integration

---
+++ bazaar/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = process.env.REACT_APP_STRIPE_KEY;
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Bazaar bazinga baz."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- bazaar/src/pages/checkout/checkout.component.jsx
+++ bazaar/src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {
   selectCartItems,
   selectCartTotal,
@@ -37,6 +38,20 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+
+      <div className="test-warning">
+        *Please use any of the following test credit cards for payment*
+        <br />
+        Visa: 4242 4242 4242 4242 - Exp: 01/45 (any future date) - CVV: 123 (any
+        3 digits)
+        <br />
+        Mastercard: 5555 5555 5555 555 - Exp: 01/55 - CVV: 456
+        <br />
+        American Express: 3782 822463 10005 - Exp: 01/90 - CVV: 1234 (any 4
+        digits)
+      </div>
+
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
